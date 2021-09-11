864f5501cb23cf4932d808f02f2ee80cdfaf7223
Alex <pauloo2941@students.ecpi.edu>
2021-09-11 16:08:19 -0400

    update application title

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'cis480-capstonelist'`, () => {
+  it(`should have as title 'cis480-capstone'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cis480-capstonelist');
+    expect(app.title).toEqual('cis480-capstone');
   });
 
   it('should render title', () => {
