4ed14a24530776e1526efc682204bddf4def6335
arenignacio <55777461+arenignacio@users.noreply.github.com>
2021-11-21 03:28:30 -0500

    Partial implementation of validation

--- src/app/components/register-user/register-user.component.ts
+++ src/app/components/register-user/register-user.component.ts
@@ -1,6 +1,8 @@
 /*
     TODO:
-      get user details:
+      -validate forms
+
+      -get user details:
         firstname
         lastname
         username
@@ -14,9 +16,10 @@
       imply 
         suspended = false
         role = 2 (user)
+
 */
 
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Component, OnInit, NgZone } from '@angular/core';
 import { CrudService } from 'src/app/service/crud.service';
@@ -30,34 +33,92 @@ import { ContractorService } from 'src/app/service/contractor.service';
 })
 export class RegisterUserComponent implements OnInit {
   isContractor: boolean = false;
-  userForm: FormGroup;
-  contractorForm: FormGroup;
+  userForm: any;
+  contractorForm: any;
 
   constructor(
-    public formBuilder: FormBuilder,     
+    private formBuilder: FormBuilder,     
     private router: Router,
     private ngZone: NgZone,
     private crudService: CrudService,
     private userService: UserService,
     private contractorService: ContractorService) { 
+
+  
+  }
+
+  ngOnInit(): void {
+    this.userForm =   this.formBuilder.group({ 
+      firstname: [''],
+      lastname: [''],
+      username: ['',  Validators.pattern(/^[^\s]+$/)],
+      password: ['', Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!#$%&?@+\-/\\"]).{8,20}$/)],
+      email: ['', [Validators.required, Validators.email]]
+    });
     this.contractorForm = this.formBuilder.group({ 
       companyname: [''],
       firstname: [''],
       lastname: [''],
       username: [''],
-      password: [''],
-      email: ['']
-    });
-
-    this.userForm = this.formBuilder.group({ 
-      firstname: [''],
-      lastname: [''],
-      username: [''],
-      password: [''],
-      email: ['']
+      password: ['', Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!#$%&?@+\-/\\"]).{8,20}$/)],
+      email: ['', [Validators.required, Validators.email]]
     })
   }
 
+  switchToUser() {
+    this.isContractor = false;
+    localStorage.setItem("isContractor", 'false');
+    console.log(`switch to user ${this.isContractor}`)
+  }
+
+  switchToContractor() {
+    this.isContractor = true;
+    localStorage.setItem("isContractor", 'true');
+  }
+
+  removeWhiteSpace(input: any) {
+    input.value = input.value.replace(/ /g, '');
+  }
+
+  //check if warning is hidden
+  isFormValid(id: string){
+    const control = this.isContractor ? this.contractorForm.get(id) : this.userForm.get(id);
+    
+    this.toggleWarning(id);
+    return control.pristine ? control.pristine : control.valid;
+  }
+
+  toggleWarning(id: string){
+    const control = this.isContractor ? this.contractorForm.get(id) : this.userForm.get(id);
+    const badPattern = control.errors?.pattern;
+    const isRequired = control.errors?.required;
+    let warningMsg = '';
+
+    if (isRequired) {
+      warningMsg = 'Required';
+    }
+
+    if (badPattern) {
+        
+      switch (badPattern.requiredPattern){
+          case '/^[^\\s]+$/':
+            warningMsg = 'Space not allowed'
+            break;
+      
+          case '(/^(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!#$%&?@+\\-/\\\\"]).{8,20}$/':
+            warningMsg = 'Invalid Password'
+            break;
+
+          default:
+            ;
+        }
+    }
+
+    return warningMsg;
+  }
+  
+
+  
   async onSubmit() {
 
     if (this.isContractor) { 
@@ -66,7 +127,6 @@ export class RegisterUserComponent implements OnInit {
         this.contractorService.login(this.contractorForm.value).subscribe( data => {
           localStorage.setItem("session", JSON.stringify(data));
           this.router.navigateByUrl('/')},
-    
     
             err => {console.log(err, "Invalid login"); }
             );
@@ -100,22 +160,18 @@ export class RegisterUserComponent implements OnInit {
 
   }
 
-  testLogin(username: string, ) {
-
+  testLogin() {
+    console.log(this.userForm,  this.userForm.get('password')?.errors);
+   
   }
 
-  ngOnInit(): void {
-  }
 
-  switchToUser() {
-    this.isContractor = false;
-    localStorage.setItem("isContractor", 'false');
-    console.log(`switch to user ${this.isContractor}`)
-  }
+ /*  get user() {
+    return {
+      firstname: this.userForm.get('firstname'), 
+      lastname:  this.userForm.get('lastname')};
 
-  switchToContractor() {
-    this.isContractor = true;
-    localStorage.setItem("isContractor", 'true');
-  }
+  } */
+
 
 }
