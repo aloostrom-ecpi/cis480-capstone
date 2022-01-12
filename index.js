1f311e982e46c29b8bdb07c96bba74e6cdff5f2d
arenignacio <55777461+arenignacio@users.noreply.github.com>
2022-01-12 12:57:24 -0500

    Initial commit for heroku

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ let express = require("express"),
   bodyParser = require("body-parser"),
   mongoDb = require("./database/db");
 
-console.log("hello world");
 require("dotenv").config();
 
 mongoose.connect(process.env.DB_STR, (err) => {
